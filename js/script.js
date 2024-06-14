f4feee96056fcfd95cf43e694f1ba2c92ffbafef
deepak <dp557277@gmail.com>
2024-06-14 17:04:50 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,47 @@
+// function resize(){
+
+// }
+
+// $(document).ready(function(){
+//     resize();
+// });
+  
+// $(window).resize(function(){
+//     resize();
+// });
+
+// $(window).on('load',function(){
+//     resize();
+// });
+
+
+
+// our work js-------------------------------------
+
+
+// const optionMenu = document.querySelector(".social-icon")
+// const selectBtn = optionMenu.querySelector(".main-icon")
+// const options = optionMenu.querySelectorAll(".option");
+
+// selectBtn.addEventListener("click", () => optionMenu.classList.toggle("active"));
+
+
+let listSection = document.querySelector(".option")
+		let isShow = true;
+		function showhidden(){
+			if (isShow) {
+				listSection.style.display="none";
+				isShow = false;
+			} else{
+				listSection.style.display="block"
+				isShow = true;
+			}
+		
+	} 
+
+
+	// ------------------------------our team---------------------------------
+
+
+
+	
